d0c3ffb4652c9e97610f9ca78766b26cc83d1f67
Kannu-Mandora <mandorakannu@gmail.com>
2023-02-26 18:25:13 +0530

    Fix: window is not defined error

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   AiFillLinkedin,
   AiFillTwitterSquare,
@@ -8,24 +8,38 @@ import {
 } from "react-icons/ai";
 
 type Props = {
-    classes: string;
-}
+  classes: string;
+};
 export default function SocialMedia({ classes }: Props) {
-  window.addEventListener("scroll", () => {
-    const socialMediaIcons = document.querySelector("#scrollToTop");
-    window.scrollY > 200 ? socialMediaIcons?.classList.add("hidden") : socialMediaIcons?.classList.remove("hidden");
-  });
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const hideOnScroll = document.getElementById("hideOnScroll");
+      window.scrollY > 200
+        ? hideOnScroll?.classList.remove("hidden")
+        : hideOnScroll?.classList.add("hidden");
+    });
+  }, []);
+
   return (
     <>
-      <div className={classes} id="scrollToTop" >
+      <div className={classes} id="hideOnScroll">
         <Link href="https://github.com/Kannu-Mandora/" target="_blank">
-        <AiFillGithub size={30} className="hover:bg-blue-200 transition-all delay-100 p-1 scale-125 rounded" />
+          <AiFillGithub
+            size={30}
+            className="hover:bg-blue-200 transition-all delay-100 p-1 scale-125 rounded"
+          />
         </Link>
         <Link href="https://www.linkedin.com/in/kannu-mandora/" target="_blank">
-        <AiFillLinkedin size={30} className="hover:bg-blue-200 transition-all delay-100 p-1 scale-125 rounded" />
+          <AiFillLinkedin
+            size={30}
+            className="hover:bg-blue-200 transition-all delay-100 p-1 scale-125 rounded"
+          />
         </Link>
         <Link href="https://twitter.com/KannuMandora" target="_blank">
-        <AiFillTwitterSquare size={30} className="hover:bg-blue-200 transition-all delay-100 p-1 scale-125 rounded" />
+          <AiFillTwitterSquare
+            size={30}
+            className="hover:bg-blue-200 transition-all delay-100 p-1 scale-125 rounded"
+          />
         </Link>
       </div>
     </>
