fc66ac7b9898b7c919f0e985a53d56b9e7162f29
Kannu-Mandora <mandorakannu@gmail.com>
2023-02-26 17:49:19 +0530

    Fix: background image in Main & Contact.tsx remove: dark mode classes

--- components/Main.tsx
+++ components/Main.tsx
@@ -7,7 +7,7 @@ export default function Main() {
   return (
     <>
       <SocialMedia classes="fixed top-1/2 bg-white px-3 h-40 flex flex-col justify-center items-center gap-5 shadow-md max-md:hidden rounded" />
-      <main className="background-image-glass flex-col-center gap-10">
+      <main className="background-image-glass flex-col-center gap-10 py-40">
         <h1 className="text-8xl">Hey, I&apos;m Kannu Mandora</h1>
         <p className="text-center text-xl leading-7 w-full text-gray-600">A Full Stack focused Web Developer building the Full Stack of Websites and Web <br /> Applications that leads to the success of the overall product. </p>
         <Button text="Projects" link="/#Projects" />
